208b6b37e87db5c35d6388061b2792285bf82630
Miroslav <mplancak@gmail.com>
2026-07-17 15:53:07 +0200

    Added SQS-triggered RiskAnalysisProcessor lambda along with the corresponding .LocalHarness, removed the RiskAnalysisProcessor Azure Function implementation.

---
+++ Functions/RiskAnalysisProcessor.LocalHarness/Program.cs
@@ -0,0 +1,56 @@
+using Amazon.Lambda.SQSEvents;
+using Amazon.Lambda.TestUtilities;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+var sqsClient = new AmazonSQSClient(Amazon.RegionEndpoint.EUNorth1);
+var queueUrl = "https://sqs.eu-north-1.amazonaws.com/465861110788/risk_analysis_queue.fifo";
+var function = new RiskAnalysisProcessor.RiskAnalysisProcessor();
+
+Console.WriteLine("Listening for order status events on risk_analysis_queue.fifo... (Ctrl+C to stop)");
+
+while (true)
+{
+    var response = await sqsClient.ReceiveMessageAsync(new ReceiveMessageRequest
+    {
+        QueueUrl = queueUrl,
+        MaxNumberOfMessages = 10,
+        WaitTimeSeconds = 20,
+        MessageAttributeNames = new List<string> { "All" },
+        MessageSystemAttributeNames = new List<string> { "All" }
+    });
+
+    foreach (var message in response.Messages ?? new List<Message>())
+    {
+        try
+        {
+            var sqsEvent = new SQSEvent
+            {
+                Records = new List<SQSEvent.SQSMessage>
+                  {
+                      new SQSEvent.SQSMessage
+                      {
+                          MessageId = message.MessageId,
+                          Body = message.Body,
+                          ReceiptHandle = message.ReceiptHandle,
+                          Attributes = message.Attributes,
+                          MessageAttributes = message.MessageAttributes?.ToDictionary(
+                              kvp => kvp.Key, kvp => new SQSEvent.MessageAttribute
+                              {
+                                  DataType = kvp.Value.DataType,
+                                  StringValue = kvp.Value.StringValue
+                              })
+                      }
+                  }
+            };
+
+            await function.FunctionHandler(sqsEvent, new TestLambdaContext());
+            await sqsClient.DeleteMessageAsync(queueUrl, message.ReceiptHandle);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Message {message.MessageId} failed: {ex.Message}");
+            // deliberately not deleted - stays on the queue, redelivered after the visibility timeout
+        }
+    }
+}
