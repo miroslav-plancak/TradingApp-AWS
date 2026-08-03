c9b68755a9f98aab71cb49e7ec8c716b0aca0edc
Miroslav <mplancak@gmail.com>
2026-08-03 18:04:07 +0200

    Refactored 8 LocalHarnesses into two generic SqsHarness and ScheduledHarness classes inside of TradingApp.LocalHarnessCore library

--- Functions/DeadLetterQueueProcessor.LocalHarness/Program.cs
+++ Functions/DeadLetterQueueProcessor.LocalHarness/Program.cs
@@ -1,76 +1,18 @@
-﻿using Amazon.Lambda.SQSEvents;
-using Amazon.Lambda.TestUtilities;
-using Amazon.SQS;
-using Amazon.SQS.Model;
+﻿using LambdaBootstrap;
 using Microsoft.Extensions.DependencyInjection;
-using Polly;
-using TradingApp.Business.Interfaces.Services;
-using TradingApp.Domain;
-using TradingApp.Infrastructure;
+using TradingApp.LocalHarnessCore;
 
 var services = new ServiceCollection();
-services.AddTradingAppLogging();
-services.AddTradingDbContext();
-services.AddDeadLetterServices();
-services.AddResiliencePolicy("DeadLetterQueueProcessor.LocalHarness");
+new Startup().ConfigureServices(services);
 var serviceProvider = services.BuildServiceProvider();
 
-var sqsClient = new AmazonSQSClient(Amazon.RegionEndpoint.EUNorth1);
-var dlqUrl = "https://sqs.eu-north-1.amazonaws.com/465861110788/CREATE_ORDER_QUEUE-DLQ.fifo";
+var queueUrl = "https://sqs.eu-north-1.amazonaws.com/465861110788/CREATE_ORDER_QUEUE-DLQ.fifo";
 
-Console.WriteLine("Listening for dead-lettered messages on CREATE_ORDER_QUEUE-DLQ.fifo... (Ctrl+C to stop)");
-
-while (true)
+await SqsHarness.RunAsync(queueUrl, serviceProvider, async (sp, sqsEvent, context) =>
 {
-    var response = await sqsClient.ReceiveMessageAsync(new ReceiveMessageRequest
-    {
-        QueueUrl = dlqUrl,
-        MaxNumberOfMessages = 10,
-        WaitTimeSeconds = 20,
-        MessageAttributeNames = new List<string> { "All" },
-        MessageSystemAttributeNames = new List<string> { "ApproximateReceiveCount" }
-    });
+    var function = sp.GetRequiredService<DeadLetterQueueProcessor.DeadLetterQueueProcessor>();
+    await function.FunctionHandler(sqsEvent, context);
+},"Listening for dead-lettered messages on CREATE_ORDER_QUEUE-DLQ.fifo... (Ctrl+C to stop)");
 
-    foreach (var message in response.Messages ?? new List<Message>())
-    {
-        try
-        {
-            using var scope = serviceProvider.CreateScope();
 
-            var function = new DeadLetterQueueProcessor.DeadLetterQueueProcessor(
-                scope.ServiceProvider.GetRequiredService<TradingDbContext>(),
-                scope.ServiceProvider.GetRequiredService<IDeadLetterService>(),
-                scope.ServiceProvider.GetRequiredService<HttpClient>(),
-                scope.ServiceProvider.GetRequiredService<IAsyncPolicy>()
-                );
 
-            var sqsEvent = new SQSEvent
-            {
-                Records = new List<SQSEvent.SQSMessage>
-                  {
-                      new SQSEvent.SQSMessage
-                      {
-                          MessageId = message.MessageId,
-                          Body = message.Body,
-                          ReceiptHandle = message.ReceiptHandle,
-                          Attributes = message.Attributes,
-                          MessageAttributes = message.MessageAttributes?.ToDictionary(
-                              kvp => kvp.Key, kvp => new SQSEvent.MessageAttribute
-                              {
-                                  DataType = kvp.Value.DataType,
-                                  StringValue = kvp.Value.StringValue
-                              })
-                      }
-                  }
-            };
-
-            await function.FunctionHandler(sqsEvent, new TestLambdaContext());
-            await sqsClient.DeleteMessageAsync(dlqUrl, message.ReceiptHandle);
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Message {message.MessageId} failed: {ex.Message}");
-            // deliberately not deleted - stays on the DLQ for inspection/retry
-        }
-    }
-}
--- Functions/ScheduledOutboxMessageProcessor.LocalHarness/Program.cs
+++ Functions/ScheduledOutboxMessageProcessor.LocalHarness/Program.cs
@@ -1,50 +1,14 @@
-using Amazon.Lambda.TestUtilities;
-using Amazon.SQS;
-using Handler.Interfaces;
-using Handler.Services;
-using Handler.Settings;
+using Handler;
+using LambdaBootstrap;
 using Microsoft.Extensions.DependencyInjection;
-using TradingApp.Infrastructure;
+using TradingApp.LocalHarnessCore;
 
 var services = new ServiceCollection();
-services.AddTradingAppLogging();
-services.AddTradingDbContext();
-services.AddTradingDbContextFactory();
-services.AddSqsClient();
-services.AddResiliencePolicy(ResiliencePolicyKey.Sql, "OutboxProcessingService-Sql");
-services.AddResiliencePolicy(ResiliencePolicyKey.Messaging, "CREATE_ORDER_QUEUE");
-
-services.AddScoped<IOutboxQuarantineService, OutboxQuarantineService>();
-services.AddScoped<IOutboxProcessingService, OutboxProcessingService>();
-services.AddScoped<IOutboxRecoveryService, OutboxRecoveryService>();
-
-services.AddSingleton<OutboxMessageProcessorSettings>();
-
+new Startup().ConfigureServices(services);
 var serviceProvider = services.BuildServiceProvider();
 
-Console.WriteLine("Simulating EventBridge Scheduler - running ScheduledOutboxMessageProcessor every 60s... (Ctrl+C to stop)");
-
-while (true)
+await ScheduledHarness.RunAsync(serviceProvider, async (sp, context) =>
 {
-    try
-    {
-        using var scope = serviceProvider.CreateScope();
-        var function = new Handler.ScheduledOutboxMessageProcessor(
-            scope.ServiceProvider.GetRequiredService<IAmazonSQS>(),
-            scope.ServiceProvider.GetRequiredService<IOutboxQuarantineService>(),
-            scope.ServiceProvider.GetRequiredService<IOutboxProcessingService>(),
-            scope.ServiceProvider.GetRequiredService<IOutboxRecoveryService>(),
-            scope.ServiceProvider.GetRequiredService<OutboxMessageProcessorSettings>()
-            );
-
-        await function.FunctionHandler(new TestLambdaContext());
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"Tick failed: {ex.Message}");
-        // deliberately not rethrown - one bad tick shouldn't kill the harness,
-        // same reasoning as the ReceiveMessageAsync guard we discussed earlier
-    }
-
-    await Task.Delay(TimeSpan.FromMinutes(1));
-}
+    var function = sp.GetRequiredService<ScheduledOutboxMessageProcessor>();
+    await function.FunctionHandler(context);
+}, "ScheduledOutboxMessageProcessor every 60s... (Ctrl+C to stop)");
